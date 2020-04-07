13254ae7cece5534abdd6b2da03e65b642e699cb
kodmunki <jacob.mulholland@kodmunki.com>
2020-04-07 16:58:02 -0500

    1.2.14

---
+++ dist/ku4-form-validation.entry.js
@@ -0,0 +1,42 @@
+import { r as registerInstance, h, g as getElement } from './index-a15aeab6.js';
+
+const Ku4FormValidation = class {
+    constructor(hostRef) {
+        registerInstance(this, hostRef);
+    }
+    /**
+     * Call to explicitly validate this validation
+     */
+    async validate() {
+        const { fields } = this;
+        const methods = fields.map(field => field.validate());
+        // eslint-disable-next-line no-undef
+        return Promise.all(methods).then(values => !values.some(v => v === false));
+    }
+    get form() {
+        return this.host.querySelector('form');
+    }
+    get fields() {
+        return [].slice.call(this.form.querySelectorAll('ku4-validation'));
+    }
+    submit() {
+        const { form } = this;
+        this.validate().then(valid => valid && form.submit());
+    }
+    componentWillLoad() {
+        const { form } = this;
+        const onsubmit = form.onsubmit || (() => { });
+        form.onsubmit = (e) => {
+            e.preventDefault();
+            this.submit();
+            onsubmit.call(form, e);
+            return false;
+        };
+    }
+    render() {
+        return h("slot", null);
+    }
+    get host() { return getElement(this); }
+};
+
+export { Ku4FormValidation as ku4_form_validation };
