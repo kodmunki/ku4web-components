c5d94e19af88961fa56497a7dd47165659554747
kodmunki <jacob.mulholland@kodmunki.com>
2020-10-15 01:33:35 -0500

    4.6.20

--- dist/ku4web-components.js
+++ dist/ku4web-components.js
@@ -116,7 +116,7 @@ var __assign=this&&this.__assign||function(){return(__assign=Object.assign||func
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-2dd63f12.system.js', new URL(resourcesUrl, window.location.origin));
+    var url = new URL('./p-394649f6.system.js', new URL(resourcesUrl, window.location.origin));
     System.import(url.href);
   };
 
