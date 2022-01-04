635e84c27a2f9575094deea656069cb4b4a82033
kodmunki <jacob.mulholland@kodmunki.com>
2022-01-04 10:23:42 -0600

    6.4.41

--- dist/ku4web-components.js
+++ dist/ku4web-components.js
@@ -114,7 +114,7 @@ DOMTokenList
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-5848719e.system.js', new URL(resourcesUrl, window.location.origin));
+    var url = new URL('./p-b7918e35.system.js', new URL(resourcesUrl, window.location.origin));
     System.import(url.href);
   };
 
